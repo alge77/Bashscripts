1f99595bda9097c189a1bc1d2e4fbf913f4846a7
Markus Richter <alge77@gmail.com>
2019-03-01 10:10:15 +0100

    Create update.sh
    init commit

---
+++ update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+TEXT_RESET='\e[0m'
+TEXT_RED_B='\e[1;31m'
+TEXT_YELLOW='\e[33;100m'
+TEXT_GREEN='\e[32;100m'
+TEXT_HIDDEN='\e[8m'
+TEXT_BLINK='\e[5m'
+
+echo -e $TEXT_GREEN
+echo '      Hello' $USER 'lets update your machine...'
+echo -e $TEXT_RESET
+
+echo -e $TEXT_HIDDEN
+sudo apt update
+echo -e $TEXT_RESET
+echo -e $TEXT_YELLOW
+echo '      APT update finished...'
+echo -e $TEXT_RESET
+
+echo -e $TEXT_HIDDEN
+sudo apt dist-upgrade -y
+echo -e $TEXT_RESET
+echo -e $TEXT_YELLOW
+echo '      APT dist-upgrade finished...'
+echo -e $TEXT_RESET
+
+echo -e $TEXT_HIDDEN
+sudo apt upgrade -y
+echo -e $TEXT_RESET
+echo -e $TEXT_YELLOW
+echo '      APT upgrade finished...'
+echo -e $TEXT_RESET
+
+echo -e $TEXT_HIDDEN
+sudo apt autoremove -y
+echo -e $TEXT_RESET
+echo -e $TEXT_YELLOW
+echo '      APT autoremove finished...'
+echo -e $TEXT_RESET
+
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo '      Reboot required!'
+    echo -e $TEXT_RESET
+fi
+
+echo -e $TEXT_GREEN
+echo -e $TEXT_BLINK
+echo '      Yeah, your machine is up to date...'
+echo -e $TEXT_RESET
